15a3255717761fcde0940879f8a36a08d69058f2
Carter <38448166+c-rter@users.noreply.github.com>
2018-11-09 22:16:57 -0500

    Robust seeds
    Relative to YDAY value 313. View comments in file for more info.

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,84 +1,205 @@
+// This file empties the Goals database and inserts the goals below
+
 const mongoose = require("mongoose");
 const db = require("../models");
-
-// This file empties the Books collection and inserts the books below
 
 mongoose.connect(
   process.env.MONGODB_URI ||
   "mongodb://localhost/21days"
 );
 
-/* {
-  title: "The Dead Zone",
-  author: "Stephen King",
-  synopsis:
-    "A number-one national best seller about a man who wakes up from a five-year coma able to see people's futures and the terrible fate awaiting mankind in The Dead Zone - a \"compulsive page-turner\" (The Atlanta Journal-Constitution). Johnny Smith awakens from a five-year coma after his car accident and discovers that he can see people's futures and pasts when he touches them. Many consider his talent a gift; Johnny feels cursed. His fiancée married another man during his coma, and people clamor for him to solve their problems. When Johnny has a disturbing vision after he shakes the hand of an ambitious and amoral politician, he must decide if he should take drastic action to change the future. The Dead Zone is a \"faultlessly paced...continuously engrossing\" (Los Angeles Times) novel of second sight.",
-  date: new Date(Date.now())
-}, */
+
+
+/* 
+
+YDAY is a 1-365 Integer of Current Day relative to First Day of Year
+
+This database is constructed based on a YDAY value of: 313 (November 9)
+
+Update your seeds relative to current YDAY
+
+*/
 
 const goalSeed = [
   {
     username: "carter",
     password: "password",
-    habit: "test habit",
-    dayCounter: 20,
-    startDay: 290,
+    habit: "Meditate Every Day",
+    dayCounter: 5,
+    startDay: 307,
     habitStatus: "active",
     rollingDay: 312
   },
   {
     username: "carter",
     password: "password",
-    habit: "test habit 2",
-    dayCounter: 4,
-    startDay: 305,
+    habit: "Practice Guitar",
+    dayCounter: 20,
+    startDay: 292,
     habitStatus: "active",
     rollingDay: 312
   },
   {
     username: "carter",
     password: "password",
-    habit: "test habit",
-    dayCounter: 20,
-    startDay: 290,
-    habitStatus: "active",
-    rollingDay: 312
-  },
-  {
-    username: "carter",
-    password: "password",
-    habit: "test habit 2",
-    dayCounter: 4,
-    startDay: 305,
-    habitStatus: "achieve",
-    rollingDay: 312
-  },
-  {
-    username: "carter",
-    password: "password",
-    habit: "test habit",
-    dayCounter: 20,
-    startDay: 290,
-    habitStatus: "achieve",
-    rollingDay: 312
-  },
-  {
-    username: "carter",
-    password: "password",
-    habit: "test habit 2",
-    dayCounter: 4,
-    startDay: 305,
+    habit: "Eat Fewer Calories",
+    dayCounter: 9,
+    startDay: 301,
     habitStatus: "fail",
+    rollingDay: 311
+  },
+  {
+    username: "carter",
+    password: "password",
+    habit: "Learn C# - 15 minutes",
+    dayCounter: 7,
+    startDay: 306,
+    habitStatus: "active",
+    rollingDay: 313
+  },
+  {
+    username: "carter",
+    password: "password",
+    habit: "Eat an apple",
+    dayCounter: 11,
+    startDay: 302,
+    habitStatus: "active",
+    rollingDay: 313
+  },
+  {
+    username: "abbas",
+    password: "password",
+    habit: "check in to Khan Academy",
+    dayCounter: 12,
+    startDay: 300,
+    habitStatus: "active",
+    rollingDay: 312
+  },
+  {
+    username: "abbas",
+    password: "password",
+    habit: "Bike to work",
+    dayCounter: 21,
+    startDay: 291,
+    habitStatus: "achieve",
+    rollingDay: 313
+  },
+  {
+    username: "abbas",
+    password: "password",
+    habit: "Go to bed by 11pm",
+    dayCounter: 5,
+    startDay: 306,
+    habitStatus: "fail",
+    rollingDay: 311
+  },
+  {
+    username: "abbas",
+    password: "password",
+    habit: "Tidy Room",
+    dayCounter: 12,
+    startDay: 301,
+    habitStatus: "active",
+    rollingDay: 313
+  },
+  {
+    username: "abbas",
+    password: "password",
+    habit: "do 20 push ups",
+    dayCounter: 10,
+    startDay: 303,
+    habitStatus: "active",
+    rollingDay: 313
+  },
+  {
+    username: "raj",
+    password: "password",
+    habit: "eat breakfast",
+    dayCounter: 5,
+    startDay: 307,
+    habitStatus: "active",
+    rollingDay: 312
+  },
+  {
+    username: "raj",
+    password: "password",
+    habit: "Practice drawing",
+    dayCounter: 21,
+    startDay: 292,
+    habitStatus: "achieve",
+    rollingDay: 313
+  },
+  {
+    username: "raj",
+    password: "password",
+    habit: "Practice algorithms",
+    dayCounter: 9,
+    startDay: 301,
+    habitStatus: "fail",
+    rollingDay: 311
+  },
+  {
+    username: "raj",
+    password: "password",
+    habit: "Talk to a new person",
+    dayCounter: 7,
+    startDay: 306,
+    habitStatus: "active",
+    rollingDay: 313
+  },
+  {
+    username: "raj",
+    password: "password",
+    habit: "Do a crossword",
+    dayCounter: 21,
+    startDay: 301,
+    habitStatus: "achieve",
+    rollingDay: 314
+  },
+  {
+    username: "nitin",
+    password: "password",
+    habit: "practice mma training",
+    dayCounter: 5,
+    startDay: 307,
+    habitStatus: "active",
     rollingDay: 312
   },
   {
     username: "nitin",
     password: "password",
-    habit: "Compliment someone daily",
-    dayCounter: 18,
-    startDay: 20,
+    habit: "Work on novel",
+    dayCounter: 20,
+    startDay: 292,
+    habitStatus: "active",
+    rollingDay: 312
+  },
+  {
+    username: "nitin",
+    password: "password",
+    habit: "Spend time with family",
+    dayCounter: 21,
+    startDay: 290,
+    habitStatus: "achieve",
+    rollingDay: 311
+  },
+  {
+    username: "nitin",
+    password: "password",
+    habit: "Learn something new",
+    dayCounter: 21,
+    startDay: 306,
+    habitStatus: "achieve",
+    rollingDay: 313
+  },
+  {
+    username: "nitin",
+    password: "password",
+    habit: "Organize house",
+    dayCounter: 10,
+    startDay: 303,
     habitStatus: "fail",
-    rollingDay: 38
+    rollingDay: 313
   }
 ];
 
